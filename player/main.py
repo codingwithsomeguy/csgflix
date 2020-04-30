8e877b1522139cd49e52b7c254fc2252750dc3cb
Some Guy <me@codingwithsomeguy.com>
2020-04-30 18:04:32 -0400

    CSGFlix E3 - Compression

--- player/main.py
+++ player/main.py
@@ -12,13 +12,29 @@ def home():
 
 import PIL.Image
 import numpy as np
+from itertools import groupby
+
+def rle_encode(arr):
+    result = []
+    for name, grp in groupby(arr):
+        #print(len(list(grp)), name)
+        result.append(len(list(grp)))
+        result.append(name)
+    return result
+
 
 @app.route("/imga/<iid>")
 def imga(iid):
     iid = int(iid)
     i1 = PIL.Image.open("static/img/m1i%03d.png" % iid)
     i1a = np.array(i1).flatten()
-    return json.dumps(i1a.tolist())
+    i1a = np.array([x >> 1 for x in i1a])
+    # Clamp:
+    i1a[i1a <= 20] = 0
+    # RLE:
+    result = rle_encode(i1a.tolist())
+    #return json.dumps(i1a.tolist())
+    return json.dumps(result)
 
 
 @app.route("/imgad/<iid>")
